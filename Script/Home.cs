3cc2f6566eba02c86c78e0daa2b7e2b44d16f366
Dhrutik Variya <dhrutikv@gmail.com>
2024-08-29 18:08:42 +0530

    Version(1.02)

--- Script/Home.cs
+++ Script/Home.cs
@@ -1,21 +1,58 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class Home : MonoBehaviour
+public class Home : MonoBehaviourPunCallbacks
 {
     [SerializeField]
-    GameObject FirstPage, RoomPage;
+    GameObject FirstPage, RoomPage, Roomlist, PlayingPage;
 
     [SerializeField]
-    TextMeshProUGUI Username;
+    TextField RoomID;
 
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            Submit();
+        }
+    }
 
     public void Submit()
     {
-        print($"Name ---> {Username.text}");
+        //PhotonNetwork.NickName = Username.text;
+        PhotonNetwork.ConnectUsingSettings();             
+    }
+
+    public void btnCreateRoom()
+    {
+        RoomPage.SetActive(false);
+        PlayingPage.SetActive(true);
+        PhotonNetwork.CreateRoom(" Hello ");
+    }
+
+    public void btnJoinRoom()
+    {
+        RoomPage.SetActive(false);
+        PlayingPage.SetActive(true);
+        PhotonNetwork.CreateRoom(" Hello ");
+    }
+
+
+    public override void OnConnected()
+    {
+        Debug.Log("OnConnected :");
+        //Connected.color = Color.green;
+
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("OnConnectedToMaster :");
         FirstPage.SetActive(false);
         RoomPage.SetActive(true);
     }
+
 }
